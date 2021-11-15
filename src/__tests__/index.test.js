de67b199a35729b0d0f08820f9dca55bfe69a2c2
edwardTunkl <eddytunkl@googlemail.com>
2021-11-15 13:17:05 +0100

    added testing

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -19,33 +19,135 @@ describe("Testing the user endpoints", () => {
       done();
     });
   });
-  it("should test users endpoint get request", async () => {
-    const response = await request.get("");
+
+  let token;
+  let testUserId;
+  
+  const validCredentials = {
+    email: "test@strive.com",
+    password: "1234"
+  }
+
+  it("should test post request for /users/account endpoint ", async () => {
+    const response = await request.post("/users/account").send({
+      username: "testUser",
+      email: "test@strive.com",
+      password: "1234",
+    });
+    expect(response.status).toBe(201);
+    token = response.body.accessToken;
+    testUserId = response.body._id;
+    expect(response.body).toBe({
+      user: {
+        username: "testUser",
+        email: "test@strive.com",
+        avatar: "",
+        _id: testUserId,
+        token: token,
+      },
+      accessToken: token,
+    });
   });
+
+  
   it("should test endpoint", async () => {
-    const response = await request.get("");
+    const response = await request.get("/users?username=testUser");
+    expect(response.status).toBe(200);
+    expect(response.body).toBe([
+      {
+        _id: testUserId,
+        username: "testUser",
+        email: "test@strive.com",
+        avatar: ""
+      }
+    ]
+    )
   });
+  
   it("should test endpoint", async () => {
-    const response = await request.get("");
+    const response = await request.get("/users?email=test@strive.com");
+    expect(response.status).toBe(200);
+    expect(response.body).toBe([
+      {
+        _id: testUserId,
+        username: "testUser",
+        email: "test@strive.com",
+        avatar: ""
+      }
+    ]
+    )
   });
+  
   it("should test endpoint", async () => {
-    const response = await request.get("");
+    const response = await request.get("/users/me").send({accessToken: token})
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(
+      {
+        _id: testUserId,
+        username: "testUser",
+        email: "test@strive.com",
+        avatar: ""
+      }
+    )
   });
+
+
   it("should test endpoint", async () => {
-    const response = await request.get("");
+    const response = await request.put("/users/me").send({user: {username: "someOtherGuy"}, accessToken: token} )
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(
+      {
+        _id: testUserId,
+        username: "someOtherGuy",
+        email: "test@strive.com",
+        avatar: ""
+      }
+    )
   });
-  it("should test endpoint", async () => {
-    const response = await request.get("");
+
+
+  it("should test endpoint for user/me/avatar", async () => {
+    const response = await request.post("/users/me/avatar").send({user: {avatar: "123123"},  accessToken: token});
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(
+      {
+        _id: testUserId,
+        username: "someOtherGuy",
+        email: "test@strive.com",
+        avatar: "123123"
+      }
+    )
+  })
+
+
+  it("should test endpoint get/users/:id", async () => {
+    const response = await request.get(`/users/${testUserId}`);
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(
+      {
+        _id: testUserId,
+        username: "someOtherGuy",
+        email: "test@strive.com",
+        avatar: "123123"
+      }
+    )
   });
-  it("should test endpoint", async () => {
-    const response = await request.get("");
+
+
+  // it("should test endpoint users/session", async () => {
+  //   const response = await request.get("/users/session").send(validCredentials)
+  //   expect(response.status).toBe(200);
+
+    
+  // });
+  
+  it("should test endpoint users/session", async () => {
+    const response = await request.get("/users/session").send(validCredentials)
+    expect(response.status).toBe(200);
+    expect(response.body.accessToken === token).toBe(false)
+    
   });
-  it("should test endpoint", async () => {
-    const response = await request.get("");
-  });
-  it("should test endpoint", async () => {
-    const response = await request.get("");
-  });
+  
   afterAll((done) => {
     mongoose.connection.dropDatabase().then(() => {
       return mongoose.connection.close().then(() => {
