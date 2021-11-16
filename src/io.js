5bb30fa80389ab4b8ec4977817c805abe2a40835
HajnalAron <hajnalaronandras@gmail.com>
2021-11-16 15:19:31 +0100

    more socket io logic added

--- src/io.js
+++ src/io.js
@@ -3,19 +3,57 @@ import { createServer } from "http";
 import { app } from "./app.js";
 import ChatSchema from "./services/chat/schema.js";
 import { verifyJWT } from "./services/auth/token.js";
+import createHttpError from "http-errors";
+import UserSchema from "./services/user/schema.js";
 
 export const httpServer = createServer(app);
 
 const io = new Server(httpServer, { allowEIO3: true });
 
+let onlineUsers = [];
+
 io.on("connection", async (socket) => {
-  const accessToken = socket.handshake.headers["access-token"];
-  const decodedToken = await verifyJWT(accessToken);
-  const chatHistory = await ChatSchema.find({
-    members: { $in: [decodedToken] }
+  try {
+    const accessToken = socket.handshake.headers["access-token"];
+
+    if (accessToken) {
+      const decodedToken = await verifyJWT(accessToken);
+
+      if (decodedToken) {
+        const chatHistory = await ChatSchema.find({
+          members: { $in: [decodedToken] }
+        });
+
+        const user = await UserSchema.findById(decodedToken);
+
+        chatHistory.map((c) => {
+          socket.join(c._id);
+          socket.emit("join", c._id);
+        });
+
+        onlineUsers.push({
+          username: user.username,
+          _id: user._id,
+          socketId: socket.id
+        });
+      }
+    } else {
+      const error = createHttpError(404, { message: "Missing access token" });
+      socket.emit("error", error);
+    }
+  } catch (error) {
+    socket.emit("error", error);
+  }
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((u) => u.socketId !== socket.id);
   });
-  chatHistory.map((c) => {
-    socket.join(c._id);
-    socket.emit("join", c._id);
-  });
+
+  //   socket.on("outgoing-msg");
+  //   socket.on("incoming-msg");
 });
+
+// connect
+// disconnect
+// outgoing-msg
+// incoming-msg
