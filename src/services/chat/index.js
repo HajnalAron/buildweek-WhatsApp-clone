390fba748d59f13d270c8c493b8181281ae65db8
emanuele <emanuelefurlani.go@gmail.com>
2021-11-16 10:57:24 +0100

    push last part

--- src/services/chat/index.js
+++ src/services/chat/index.js
@@ -8,12 +8,18 @@ const chatRouter = express.Router()
 chatRouter.post("/", JWTAuthMiddleware, async(req, res, next) => {
     try {
         //First we have 2 id: one is the sender (req.user.id) and one the reciver ()
-        const myId = req.user._id
-        const membersArr = req.body.members
+        const requesterId = req.user._id
+        const reciverId = req.body.reciverId
+        const message  = req.body.message
+       
         
         const commonChat = await ChatSchema.find(
-            { members: { $in: [membersArr, myId ] }})
+            // { members: { $in: [membersArr, myId ] }})
             // {$and: [{members: {oneID}}, {members: {otherID}}]
+            {$and:[{ members: { $in: [ requesterId ] }} , {members:{$in:[reciverId]}} ] })
+            if(commonChat && commonChat.members.length === 2) {
+                ChatSchema.findOneAndUpdate({_id:commonChat._id},{$push:{history: message}})
+            }
     } catch (error) {
         next(error)
     }
