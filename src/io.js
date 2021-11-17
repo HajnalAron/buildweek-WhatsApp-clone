0058929188591465a743b5245e0af766668e687c
HajnalAron <hajnalaronandras@gmail.com>
2021-11-17 14:20:53 +0100

    socket.io backend finished

--- src/io.js
+++ src/io.js
@@ -5,6 +5,7 @@ import ChatSchema from "./services/chat/schema.js";
 import { verifyJWT } from "./services/auth/token.js";
 import createHttpError from "http-errors";
 import UserSchema from "./services/user/schema.js";
+import { request } from "express";
 
 export const httpServer = createServer(app);
 
@@ -20,75 +21,78 @@ io.on("connection", async (socket) => {
       const decodedToken = await verifyJWT(accessToken);
 
       if (decodedToken) {
-        const chatHistory = await ChatSchema.find({
+        const chatRooms = await ChatSchema.find({
           members: { $in: [decodedToken] }
         });
 
-        const user = await UserSchema.findById(decodedToken);
-
-        chatHistory.map((c) => {
-          socket.join(c._id);
-          socket.emit("join", c._id);
+        chatRooms.map((r) => {
+          socket.join(r._id);
+          socket.emit("join", r._id);
         });
+
+        const user = await UserSchema.findById(decodedToken);
 
         onlineUsers.push({
           username: user.username,
           _id: user._id,
           socketId: socket.id
         });
+        console.log(onlineUsers);
       }
     } else {
       const error = createHttpError(404, { message: "Missing access token" });
-      socket.emit("error", error);
+      socket.emit("disconnect", error);
     }
   } catch (error) {
     socket.emit("error", error);
+    socket.disconnect();
   }
 
   socket.on("disconnect", () => {
     onlineUsers = onlineUsers.filter((u) => u.socketId !== socket.id);
   });
 
-  socket.on("outgoing-msg", async ({message, requestTargetId})=> {
+  socket.on("outgoing-msg", async (requestData) => {
     try {
-      console.log(message)
-    const accessToken = socket.handshake.headers["access-token"];
-    const decodedToken = await verifyJWT(accessToken);
-    const requesterId = decodedToken;
-    const reciverId = mongoose.Types.ObjectId(requestTargetId);
-    message.sender = requesterId
-    const commonChat = await ChatSchema.find({
-      $and: [
-        { members: { $in: [requesterId] } },
-        { members: { $in: [reciverId] } }
-      ]
-    });
-    // console.log(commonChat);
+      const { message, requestTargetId } = requestData;
+      const accessToken = socket.handshake.headers["access-token"];
+      const decodedToken = await verifyJWT(accessToken);
+      message.sender = decodedToken;
 
-    if (commonChat.length > 0) {
-      const updatedCommonChat = await ChatSchema.findByIdAndUpdate(
-        commonChat[0]._id,
-        { $push: { history: message } },
-        { new: true }
-      );
-      console.log(updatedCommonChat)
-        //1. send the new chat data to the other client, socket.to(clinetID).emit("incoming msg ", messageData)
-        //2. make the other client refetch the message data socket.to(clientId).emit("incongi message", id of the chat)
-    } else {
-      const newChat = await new ChatSchema({
-        members: [reciverId, requesterId],
-        history: [message]
-      }).save();
+      const commonChat = await ChatSchema.find({
+        $and: [
+          { members: { $in: [decodedToken] } },
+          { members: { $in: [requestTargetId] } }
+        ]
+      });
+
+      if (commonChat.length > 0) {
+        const updatedCommonChat = await ChatSchema.findByIdAndUpdate(
+          commonChat[0]._id,
+          { $push: { history: message } },
+          { new: true }
+        );
+        socket
+          .to(updatedCommonChat._id)
+          .emit("incoming-msg", updatedCommonChat);
+      } else {
+        const newChat = await new ChatSchema({
+          members: [reciverId, requesterId],
+          history: [message]
+        }).save();
+        const targetSocketId = onlineUsers.find(
+          (user) => user._id === requestTargetId
+        );
+        socket.to(targetSocketId).emit("join", newChat._id);
+        socket.to(newChat._id).emit("incoming-msg", newChat);
+      }
+    } catch (error) {
+      socket.emit("error", error);
     }
-  } catch (error) {
-    socket.emit("error", error)
-  }
-});
   });
+});
 
-
-  // socket.on("incoming-msg")   
-
+// socket.on("incoming-msg")
 
 // connect
 // disconnect
